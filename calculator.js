01ac5afabcc19f535d4347c1f9c59ee56aa90d5e
Melissa <mj.eaton3@gmail.com>
2016-02-01 21:22:28 -0500

    fixed janky code

--- calculator.js
+++ calculator.js
@@ -1,78 +1,64 @@
-$(document).ready(function() {
+var stringArray;
+var stringArray1;
+var stringArray2;
+var temp;
+var output = "";
+var usedIndex = [];
+operationsArray = ["*", "/", "%", "+", "-"];
 
-	var stringArray;
-	var stringArray1;
-	var stringArray2;
-	var temp;
-	var output = "";
-	var usedIndex = [];
-	operationsArray = ["*", "/", "%", "+", "-"];
+var counter = 0;
+var output = "";
+var tempValue;
+var tempOperator1;
+var tempOperator2;
 
-	var counter = 0;
-	$("button").on("click", function(){
-		if (counter === 0 && !$(this).hasClass("number")) {
-			return;
-		} else if ($(this).hasClass("number") || $(this).hasClass("operation")){
-			output += this.value;
-			$("#result").val(output);
-			counter++;
-		} else if (this.value === "clearEverything") {
+$("button").on("click", function(){
+	var number = $(this).hasClass("number");
+	var operator = $(this).hasClass("operator");
+	if (number) {
+		output += this.value;
+		$("#result").val(output);
+	} else if (operator) {
+		tempOperator2 = this.value;
+		if (isNaN(tempValue)) {
+			//if there is a tempOperator and NOT a tempValue = not ready to evalue
+			tempValue = parseFloat(output);
 			output = "";
-			counter = 0;
-			indexUsed = [];
-			$("#result").val(output);
+			tempOperator = this.value;
+			$("#result").val(tempOperator);
+			tempOperator1 = tempOperator2;
+		} else {
+			tempOperator2 = this.value;
+			//if there is a tempOperator AND a tempValue = ready to evaluate
+			tempValue = evaluateAll(tempValue, tempOperator1, output);
+			output = "";
+			$("#result").val(tempValue);
+			tempOperator1 = tempOperator2;
 		}
-		//when equal sign is clicked
-		if (this.value === " = ") {
-			stringArray = output.split(" ");
-			for (var i = 0; i < stringArray.length; i++) {
-				if (stringArray[i] === "*") {
-					if (i === 1) {
-						temp = parseFloat(stringArray[i-1]) * parseFloat(stringArray[i+1]);
-						stringArray = stringArray.splice(i+2) + temp;
-					} else {
-						temp = parseFloat(stringArray[i-1]) * parseFloat(stringArray[i+1]);
-						stringArray = stringArray.splice(0, i-2) + temp + stringArray.splice(i+2);
-					}
-				} else if (stringArray[i] === "/") {
-					if (i === 1) {
-						temp = parseFloat(stringArray[i-1]) / parseFloat(stringArray[i+1]);
-						stringArray = stringArray.splice(i+2) + temp;
-					} else {
-						temp = parseFloat(stringArray[i-1]) / parseFloat(stringArray[i+1]);
-						stringArray = stringArray.splice(0, i-2) + temp + stringArray.splice(i+2);
-					}
-				} else if (stringArray[i] === "%") {
-					if (i === 1) {
-						temp = parseFloat(stringArray[i-1]) % parseFloat(stringArray[i+1]);
-						stringArray = stringArray.splice(i+2) + temp;
-					} else {
-						temp = parseFloat(stringArray[i-1]) % parseFloat(stringArray[i+1]);
-						stringArray = stringArray.splice(0, i-2) + temp + stringArray.splice(i+2);
-					}
-				}
-			}
-			for (var j = 0; j < stringArray.length; j++) {
-				if (stringArray[j] === "+") {
-					if (j === 1) {
-						temp = parseFloat(stringArray[j-1]) + parseFloat(stringArray[j+1]);
-						stringArray = stringArray.splice(j+2) + temp;
-					} else {
-						temp = parseFloat(stringArray[j-1]) + parseFloat(stringArray[j+1]);
-						stringArray = stringArray.splice(0, j-2) + temp + stringArray.splice(j+2);
-					}
-				} else if (stringArray[j] === "-") {
-					if (j === 1) {
-						temp = parseFloat(stringArray[j-1]) - parseFloat(stringArray[j+1]);
-						stringArray = stringArray.splice(j+2) + temp;
-					} else {
-						temp = parseFloat(stringArray[j-1]) - parseFloat(stringArray[j+1]);
-						stringArray = stringArray.splice(0, j-2) + temp + stringArray.splice(j+2);
-					}
-				}
-			}
-			output = stringArray;
-			$("#result").val(output);
-		}
-	});
+	} else if (this.value === "clearEverything") {
+		output = "", tempValue = undefined, tempOperator = undefined;
+		$("#result").val(output);
+	} else if (this.value === " = ") {
+		tempValue = evaluateAll(tempValue, tempOperator2, output);
+		output = tempValue;
+		$("#result").val(output);
+		tempValue = undefined, tempOperator1 = undefined, tempOperator2 = undefined;
+	}
 });
+
+function evaluateAll(tempValue, tempOperator, output) {
+	var num1 = tempValue;
+	var num2 = parseFloat(output);
+	// console.log(num1 + tempOperator + num2);
+	if (tempOperator === " + ") {
+		return num1+num2;
+	} else if (tempOperator === " - ") {
+		return num1-num2;
+	} else if (tempOperator === " * ") {
+		return num1*num2;
+	} else if (tempOperator === " / ") {
+		return num1/num2;
+	} else if (tempOperator === " % ") {
+		return num1%num2;
+	}
+}
